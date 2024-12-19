d6c64438b879146bcdf3c249d759465eee4468be
bamb421 <84636375+bamb421@users.noreply.github.com>
2024-12-19 21:07:31 +1300

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -7,8 +7,25 @@ local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/bamb4
 local entity = spawner.Create({
 	Entity = {
 		Name = "Template Entity",
-		Asset = "https://github.com/RegularVynixu/Utilities/raw/main/Doors/Entity%20Spawner/Assets/Entities/Rush.rbxm",
+		Asset = "https://github.com/RegularVynixu/Utilities/blob/main/Doors%20Entity%20Spawner/Models/Rush.rbxm?raw=true",
 		HeightOffset = 0
+	},
+	Movement = {
+		Speed = 100,
+		Delay = 2,
+		Reversed = false
+	},
+	Damage = {
+		Enabled = true,
+		Range = 40,
+		Amount = 125
+	},
+	Rebounding = {
+		Enabled = true,
+		Type = "Ambush", -- "Blitz"
+		Min = 2,
+		Max = 4,
+		Delay = 2
 	},
 	Lights = {
 		Flicker = {
@@ -18,39 +35,17 @@ local entity = spawner.Create({
 		Shatter = true,
 		Repair = false
 	},
-	Earthquake = {
-		Enabled = true
-	},
-	CameraShake = {
-		Enabled = true,
-		Range = 100,
-		Values = {1.5, 20, 0.1, 1} -- Magnitude, Roughness, FadeIn, FadeOut
-	},
-	Movement = {
-		Speed = 100,
-		Delay = 2,
-		Reversed = false
-	},
-	Rebounding = {
-		Enabled = true,
-		Type = "Ambush", -- "Blitz"
-		Min = 1,
-		Max = 1,
-		Delay = 2
-	},
-	Damage = {
-		Enabled = true,
-		Range = 40,
-		Amount = 125
-	},
 	Crucifixion = {
+		Type = "Curious", -- "Guiding"
 		Enabled = true,
 		Range = 40,
 		Resist = false,
 		Break = true
+	},
+	Death = {
+		Cause = ""
 	}
 })
-
 ---====== Debug entity ======---
 
 entity:SetCallback("OnSpawned", function()
